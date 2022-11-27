235bd621b0f9d8ecf398888c2b40eb0601c005a4
KManu <kobbykmanu7@gmail.com>
2022-11-27 13:58:24 +0000

    - Added GRPC controller endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,8 @@
+import { Metadata, ServerUnaryCall } from '@grpc/grpc-js';
 import { Controller, Get } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
 import { AppService } from './app.service';
+import { ContentManagerResponse, TestID } from './reddit-crawler/crawler.interfaces';
 
 @Controller()
 export class AppController {
@@ -8,6 +11,18 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @GrpcMethod('ContentManagerService', 'HelloThere')
+  helloThere(data: TestID, metadata: Metadata, call: ServerUnaryCall<any, any>): ContentManagerResponse {
+    const items = [
+      { id: 1, name: 'John' },
+      { id: 2 },
+    ];
+    return {
+      status: 200,
+      data: items
+    };
   }
 
   
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { grpcClientOptions } from './grpc-client-options';
+
+/* async function bootstrap() {
+  let config: ConfigService = new ConfigService();
+  const app = await NestFactory.create(AppModule);
+  await app.listen(config.get<string>('PORT'));
+}
+bootstrap(); */
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, grpcClientOptions);
+  await app.listen();
 }
 bootstrap();
